e4dd1e37ee176cfc80bc6029deaf7ca995d6a0ee
vishal KUshwaha <kashyapvishal1001@gmail.com>
2026-03-22 17:36:35 +0530

    install tailwind

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from "./Navbar.module.css"
 
 function Navbar() {
   return (
-    <div className={styles.mainContainer}>Navbar</div>
+    <div className={styles.NavContainer}>Navbar</div>
   )
 }
 
